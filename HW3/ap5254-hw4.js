01539e578ad3bfc38f72d014c0e9939bc0b6ad75
Anil Poonai <50726395+DevonARP@users.noreply.github.com>
2023-04-20 15:33:51 -0400

    Add files via upload

--- HW3/ap5254-hw4.js
+++ HW3/ap5254-hw4.js
@@ -35,18 +35,12 @@ var x = db.restaurants.find({grades : { $elemMatch:{"score":{$lt : 80}}}})
 x.count()
 //Just in case the corrections are wrongly worded, the line below gets all of the documents that have a value between 80 and 100
 db.restaurants.aggregate([{$match: {"grades.score" : {$gt:80, $lt:100} } }])
-//Question 11, with the corrections asked for, it has no results
-db.restaurants.aggregate([{$match: {"address.coord.1" : {$lte:-73.9} } }])
-//I did this one just in case the coordinates were mixed up on brightspace, I know Mongo prefers tehm switched as well
+//Question 11, with the corrections asked for, longitude is first in the coordinates, guessing because mongo prefers that
 db.restaurants.aggregate([{$match: {"address.coord.0" : {$lte:-73.9} } }])
 //Question 12, American has a space behind it in the data for some reason
-db.restaurants.aggregate([{$match: {"address.coord.1" : {$lt:-65.754168},"cuisine" : {$ne: "American "}, "grades.score" : {$gt: 70} } }])
-//Same as previous questiosn reasoning
-db.restaurants.aggregate([{$match: {"address.coord.0" : {$lt:-65.754168},"cuisine" : {$ne: "American "}, "grades.score" : {$gt: 70} } }])
-//Question 13, Can't find any for the specified criteria
-db.restaurants.find({"address.coord.1": {$lt:-65.754168},"cuisine" : {$ne: "American "},"grades.score" : {$gt: 70}},{"restaurant_id" : 1,"name":1,"address":1})
-//Same as previous 2 questiosn reasoning
-db.restaurants.find({"address.coord.0": {$lt:-65.754168},"cuisine" : {$ne: "American "},"grades.score" : {$gt: 70}},{"restaurant_id" : 1,"name":1,"address":1})
+db.restaurants.find({$and: [{"address.coord.0" : {$lt:-65.754168}},{"cuisine" : {$ne: "American "}},{"grades.score" : {$gt: 70}}]})
+//Question 13,
+db.restaurants.find({"address.coord.0": {$lt:-65.754168},"cuisine" : {$ne: "American "},"grades.score" : {$gt: 70}})
 //Question 14
 db.restaurants.aggregate([{$match: {"cuisine" : {$ne: "American "}, "grades.grade" : "A", "borough" : {$ne: "Brooklyn"} } }]).sort({"cuisine":-1})
 //Question 15
@@ -69,38 +63,43 @@ db.restaurants.find({$or: [{"name": /^Wil/},{$and: [{"cuisine" : {$ne :"American
 db.restaurants.find({"grades.date":ISODate("2014-08-11T00:00:00Z"), "grades.grade":"A", "grades.score" : 11.},{restaurant_id:1, name:1, grades:1})
 //Question 24
 db.restaurants.find({"grades.1.date":ISODate("2014-08-11T00:00:00Z"), "grades.1.grade":"A", "grades.1.score" : 9.},{restaurant_id:1, name:1, grades:1})
-//Question 25
+//Question 25,
 db.restaurants.find({"address.coord.1": {$gt : 42, $lte : 52}},{"restaurant_id" : 1,"name":1,"address":1})
 
 
 //Part 2
-//Question 1
-//So there isn't any data for the filters given in the question, so I'm changing the field and value taht was mentioned to Type_Description and 1 - Restaurant
-//Created a new collection with the filtered data
-db.durham.find({"Type_Description": "1 - Restaurant","Seats":{$gte:100} }).forEach(function(x) { db.output.insert({"_id":x._id,"loc":{"type":"Point","coordinates":[x.geolocation,x.field23]},"ID":x.ID})})
+//So there isn't any data for the filters given in the question, so I'm changing the field and value taht was mentioned to Food Service
+//Created a new collection with the filtered data, put longitude first since mongo prefers that
+db.durham.find({"Rpt_Area_Desc": "Food Service","Seats":{$gte:100},field23: { $exists: true } }).forEach(function(x) { db.output.insert({"_id":x._id,"loc":{"type":"Point","coordinates":[x.field23,x.geolocation]},"ID":x.ID})})
+db.output.find().sort({"loc.coordinates.0":-1,"loc.coordinates.1":-1}).forEach(function(x) { print(x.loc.coordinates)})
+//Copied and pasted the data from the above line into python and made the coordiantes into a convex hull which got me the key points to make the polygon needed for this question
 //Used 2dsphere to get the coordinates in order
-db.output.createIndex( { geolocation : "2dsphere" } )
-//Center Point
-//35.9618939, -78.9779035
-db.output.find().skip(db.output.find().count()/2).limit(1)
-//Question 2
-//Used 2dsphere to get the coordinates in order
+db.output.createIndex( { loc : "2dsphere" } )
 db.foreclosures.createIndex( { geometry : "2dsphere" } )
-//Had to switch the positions because MongoDB and the data had it with longitude first, can switch that back if I understood that incorrectly
-db.foreclosures.find({geometry:{$geoWithin: { $centerSphere: [ [ -78.9779035, 35.9618939 ], 10/3958.8 ] } }})
-db.foreclosures.find({geometry:{$geoWithin: { $centerSphere: [ [ -78.9779035, 35.9618939 ], 10/3958.8 ] } }}).count()
+db.foreclosures.find({geometry: {$geoIntersects: {$geometry:{"type": "Polygon","coordinates": [[
+[-78.9825183, 36.0322856],
+[-79.0057432, 35.9067074],
+[-78.8667135, 35.8703056],
+[-78.8388266, 35.8767831],
+[-78.8108783, 35.9205016],
+[-78.8264275, 36.0984257],
+[-78.8482093, 36.1027227],
+[-78.9114595, 36.0765775],
+[-78.9825183, 36.0322856]
+]]}}}})
+
 
 
 //Extra Credit
-//Cleaning up the worldcities file, had to switch lat and long, can switch it back if I understood that incorrectly
+//Cleaning up the worldcities file, put longitude first since mongo prefers it
 db.cities.find().forEach(function(x) { db.newcities.insert({"_id":x._id,"city":x.city,"city_ascii":x.city_ascii,"loc":{"type":"Point","coordinates":[x.lng,x.lat]},"country":x.country,"capital":x.capital,"id":x.id})})
 //Making the datetime in readable format
 db.meteorites.aggregate([{ $addFields: { date: { $toDate: "$year" } } },{$out:"output2"}])
 //Giving the coordinates an index
 db.newcities.createIndex( { loc : "2dsphere" } )
-db.meteorites.createIndex( { geolocation : "2dsphere" } )
 db.output2.createIndex( { geolocation : "2dsphere" } )
 //Nearest City
 db.output2.find({fall: 'Fell',geolocation: { $exists: true },date: { $gte: ISODate("1950-01-01T00:00:00.000Z")}}).forEach(function(x) { print("Meteorite:", x.name, x.geolocation, db.newcities.find({loc:{$near:{$geometry: {"type":"Point","coordinates":x.geolocation.coordinates}}}},{city:1,county:1,loc:1}).limit(1))})
+
 
 
